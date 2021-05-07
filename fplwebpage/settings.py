e0b58a27093f710760ca2cbd305f42991828ca92
Ole Martin Borge <olemartinbo@gmail.com>
2021-05-07 21:46:49 +0200

    Include https and coockie secure.

--- fplwebpage/settings.py
+++ fplwebpage/settings.py
@@ -54,9 +54,9 @@ MIDDLEWARE = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') ##specify static root
 
-SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False
 
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 
 ROOT_URLCONF = 'fplwebpage.urls'
 
