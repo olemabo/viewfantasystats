18718dc7596551211fe6f9200895a6fb79c78afa
Ole Martin Borge <olemartinbo@gmail.com>
2021-08-01 19:13:25 +0200

    Changed view-controller structure. Heavy computations are called from api from javascript in main view. Loading spinner is added. Name of the website is changed. General cleanup.

---
+++ fixture_planner/static/js/fixture_planner.js
@@ -0,0 +1,26 @@
+
+//const spinnerBox = document.getElementById('spinner-box')
+//const dataBox = document.getElementById('data-box')
+
+// console.log(spinnerBox)
+// console.log(dataBox)
+
+/* $.ajax({
+    type: 'GET',
+    url: 'post-json',
+    success: function(response){
+    setTimeout(()=>{
+         spinnerBox.classList.add('not-visible')
+         dataBox.classList.add('visible')
+         dataBox.classList.remove('not-visible')
+         console.log('response', response)
+         for (const item of response){
+            dataBox.innerHTML += `<b>${item.gameweek}</b>`
+         }
+    }, 2000)
+
+    },
+    error: function(error){
+        console.log(error)
+    }
+}) */
--- fixture_planner/views.py
+++ fixture_planner/views.py
@@ -4,26 +4,12 @@ from fixture_planner.models import AddPlTeamsToDB, KickOffTime
 import fixture_planner.read_data as read_data
 from datetime import date
 import fixture_planner.fixture_algorithms as alg
-
+from django.http import JsonResponse
+from django.views.generic import TemplateView
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-
-def test(request):
-    teams = 'Hei'
-
-    context = {
-        'teams': teams,
-    }
-    return render(request, 'fixture_planner_main.html', context=context)
-
-
-def fixture_planner2(request):
-    teams = 'Hei'
-
-    context = {
-        'teams': teams,
-    }
-    return render(request, 'fixture_planner.html', context=context)
 
 class team_info:
     def __init__(self, opponent_team_name, difficulty_score, H_A, team_name, FDR_score):
@@ -34,6 +20,17 @@ class team_info:
         self.team_name = team_name
         self.FDR_score = FDR_score
         self.Use_Not_Use = 0
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+
+class which_team_to_check:
+    def __init__(self, team_name, checked, checked_must_be_in_solution=''):
+        ...
+        self.team_name = team_name
+        self.checked = checked
+        self.checked_must_be_in_solution = checked_must_be_in_solution
 
 
 def get_current_gw():
@@ -52,19 +49,12 @@ def get_current_gw():
 def get_max_gw():
     return 38
 
-class which_team_to_check:
-    def __init__(self, team_name, checked, checked_must_be_in_solution=''):
-        ...
-        self.team_name = team_name
-        self.checked = checked
-        self.checked_must_be_in_solution = checked_must_be_in_solution
-
 
 def fixture_planner(request, start_gw=get_current_gw(), end_gw=get_current_gw()+5, combinations="FDR", teams_to_check=2, teams_to_play=1, min_num_fixtures=4):
-    """View function for home page of site."""
-    # Generate counts of some of the main objects
+
     if end_gw > get_max_gw():
         end_gw = get_max_gw()
+
     # collect data from database [[AddPlTeamsToDB], [AddPlTeamsToDB], ... ]
     fixture_list_db = AddPlTeamsToDB.objects.all()
     team_name_list = []
@@ -89,6 +79,7 @@ def fixture_planner(request, start_gw=get_current_gw(), end_gw=get_current_gw()+
         end_gw = int(gw_info[1])
 
         combinations = request.POST.getlist('combination')[0]
+
         min_num_fixtures = int(request.POST.getlist('min_num_fixtures')[0])
 
         teams_to_check = int(request.POST.getlist('teams_to_check')[0])
@@ -110,8 +101,74 @@ def fixture_planner(request, start_gw=get_current_gw(), end_gw=get_current_gw()+
             fixture_list.append(fixture_list_db[i])
     teams = len(fixture_list)
 
+    context = {
+        'teams': teams,
+        'gws': gws,
+        'gw_numbers': kickofftime_db,
+        'gw_start': start_gw,
+        'gw_end': end_gw,
+        'combinations': combinations,
+        'teams_to_play': teams_to_play,
+        'teams_to_check': teams_to_check,
+        'min_num_fixtures': min_num_fixtures,
+        'team_name_list': team_name_list,
+        'number_of_teams': number_of_teams,
+        'fixture_list': fixture_list,
+        'fpl_teams': json.dumps(fpl_teams),
+    }
+
+    return render(request, 'fixture_planner2.html', context=context)
+
+
+@csrf_exempt
+def get_fdr_data(request):
+
+    if request.is_ajax():
+        if request.method == 'POST':
+            my_json = request.body.decode('utf8').replace("'", '"')
+            data = json.loads(my_json)
+
     fdr_fixture_data = []
+
+    start_gw = data['start_gw']
+    end_gw = data['end_gw']
+    fpl_teams = data['fpl_teams']
+    combinations = data['combinations']
+    min_num_fixtures = data['min_num_fixtures']
+    gws = end_gw - start_gw + 1
+    gw_numbers = [ n for n in range(start_gw, end_gw + 1)]
+
+    fixture_list_db = AddPlTeamsToDB.objects.all()
+    team_dict = {}
+    number_of_teams = len(fixture_list_db)
+
+    fixture_list = [fixture_list_db[i] for i in range(0, number_of_teams)]
+
+    for i in range(number_of_teams):
+        if fpl_teams[0] == -1:
+            team_dict[fixture_list[i].team_name] = which_team_to_check(fixture_list[i].team_name, 'checked')
+        else:
+            team_dict[fixture_list[i].team_name] = which_team_to_check(fixture_list[i].team_name, '')
+
+    if fpl_teams[0] != -1:
+        for fpl_team in fpl_teams:
+            team_dict[fpl_team] = which_team_to_check(team_dict[fpl_team].team_name, 'checked')
+
+    fixture_list_db = AddPlTeamsToDB.objects.all()
+
+    team_name_list = []
+
+    fixture_list = []
+    for i in range(number_of_teams):
+        temp_object = team_dict[fixture_list_db[i].team_name]
+        team_name_list.append(team_dict[fixture_list_db[i].team_name])
+
+        if temp_object.checked == 'checked':
+            fixture_list.append(fixture_list_db[i])
+    teams = len(fixture_list)
+
     if combinations == 'FDR':
+        fdr_fixture_data = []
         FDR_scores = []
         for idx, i in enumerate(fixture_list):
             fdr_dict = alg.create_FDR_dict(i)
@@ -129,17 +186,68 @@ def fixture_planner(request, start_gw=get_current_gw(), end_gw=get_current_gw()+
                 if temp_gw in gw_numbers:
                     temp_list2[gw_numbers.index(temp_gw)].append([
                         team_info(team_i.oppTeamNameList[j],
-                                                   team_i.oppTeamDifficultyScore[j],
-                                                   team_i.oppTeamHomeAwayList[j],
-                                                   team_i.team_name,
-                                                   FDR_score)
+                                  team_i.oppTeamDifficultyScore[j],
+                                  team_i.oppTeamHomeAwayList[j],
+                                  team_i.team_name,
+                                  FDR_score).toJson()
                     ])
+
             for k in range(len(temp_list2)):
                 if not temp_list2[k]:
-                    temp_list2[k] = [[team_info("-", 0, " ", team_i.team_name, 0)]]
+                    temp_list2[k] = [[team_info("-", 0, " ", team_i.team_name, 0).toJson()]]
 
             fdr_fixture_data.append(temp_list2)
 
+
+    if abs(min_num_fixtures) > (end_gw-start_gw):
+            min_num_fixtures = abs(end_gw-start_gw)
+            if min_num_fixtures == 0:
+                min_num_fixtures = 1
+                end_gw = start_gw + 1
+
+    if combinations == 'FDR-best':
+        fdr_fixture_data = alg.find_best_fixture_with_min_length_each_team(fixture_list, GW_start=start_gw, GW_end=end_gw, min_length=min_num_fixtures)
+
+    data = {
+        "fdr_fixture_data": fdr_fixture_data
+    }
+    return JsonResponse(data, safe=False)
+
+
+@csrf_exempt
+def get_rotation_data(request):
+    if request.is_ajax():
+        if request.method == 'POST':
+            my_json = request.body.decode('utf8').replace("'", '"')
+            data = json.loads(my_json)
+
+    start_gw = data['start_gw']
+    end_gw = data['end_gw']
+    fpl_teams = data['fpl_teams']
+    combinations = data['combinations']
+    teams_to_check = data['teams_to_check']
+    teams_to_play = data['teams_to_play']
+
+    fixture_list_db = AddPlTeamsToDB.objects.all()
+    team_dict = {}
+    number_of_teams = len(fixture_list_db)
+
+    fixture_list = [fixture_list_db[i] for i in range(0, number_of_teams)]
+
+    for i in range(number_of_teams):
+        team_dict[fixture_list[i].team_name] = which_team_to_check(fixture_list[i].team_name, '')
+
+    for fpl_team in fpl_teams:
+        team_dict[fpl_team] = which_team_to_check(team_dict[fpl_team].team_name, 'checked')
+
+    team_name_list = []
+    fixture_list = []
+    for i in range(number_of_teams):
+        temp_object = team_dict[fixture_list_db[i].team_name]
+        team_name_list.append(team_dict[fixture_list_db[i].team_name])
+
+        if temp_object.checked == 'checked':
+            fixture_list.append(fixture_list_db[i])
 
     rotation_data = []
     if combinations == 'Rotation':
@@ -156,44 +264,21 @@ def fixture_planner(request, start_gw=get_current_gw(), end_gw=get_current_gw()+
             if i.team_name in teams_in_solution:
                 i.checked_must_be_in_solution = 'checked'
         rotation_data = alg.find_best_rotation_combos2(fixture_list_db, start_gw, end_gw,
-                    teams_to_check=teams_to_check, teams_to_play=teams_to_play,
-                    team_names=fpl_teams, teams_in_solution=teams_in_solution, teams_not_in_solution=[],
-                    top_teams_adjustment=False, one_double_up=False,
-                    home_away_adjustment=True, include_extra_good_games=False,
-                                    num_to_print=0)
+                                                       teams_to_check=teams_to_check, teams_to_play=teams_to_play,
+                                                       team_names=fpl_teams, teams_in_solution=teams_in_solution,
+                                                       teams_not_in_solution=[],
+                                                       top_teams_adjustment=False, one_double_up=False,
+                                                       home_away_adjustment=True, include_extra_good_games=False,
+                                                       num_to_print=0)
         if rotation_data == -1:
             rotation_data = [['Wrong input', [], [], 0, 0, [[]]]]
         else:
             rotation_data = rotation_data[:(min(len(rotation_data), 50))]
 
-    if abs(min_num_fixtures) > (end_gw-start_gw):
-            min_num_fixtures = abs(end_gw-start_gw)
-            if min_num_fixtures == 0:
-                min_num_fixtures = 1
-                end_gw = start_gw + 1
-    if combinations == 'FDR-best':
-        fdr_fixture_data = alg.find_best_fixture_with_min_length_each_team(fixture_list, GW_start=start_gw, GW_end=end_gw, min_length=min_num_fixtures)
-
-
-    context = {
-        'teams': teams,
-        'gws': gws,
-        'gw_numbers': kickofftime_db,
-        'gw_start': start_gw,
-        'gw_end': end_gw,
-        'combinations': combinations,
-        'rotation_data': rotation_data,
-        'teams_to_play': teams_to_play,
-        'teams_to_check': teams_to_check,
-        'fdr_fixture_data': fdr_fixture_data,
-        'min_num_fixtures': min_num_fixtures,
-        'team_name_list': team_name_list,
+    data = {
+        "fdr_fixture_data": rotation_data
     }
-    # Render the HTML template index_catalog.html with the data in the context variable
-    return render(request, 'fixture_planner.html', context=context)
-
-
-
+    return JsonResponse(data, safe=False)
 
 
 def fill_data_base(request):
