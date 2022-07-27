e858584601e74c6ce54a8c335bb7ff1ff6f625d6
Ole Martin Borge <olemartinbo@gmail.com>
2022-07-27 19:44:09 +0200

    bug

--- fixture_planner_eliteserien/urls.py
+++ fixture_planner_eliteserien/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 
 
 urlpatterns = [
+    # APIs used to extract data to frontend
     path("get-eliteserien-kickoff-times/", views.GetKickOffTimesEliteserien.as_view()),
-    path("get-all-eliteserien-fdr-data/", views.GetAllEliteserienFDRData.as_view()),
+    path("get-all-eliteserien-fdr-data/", views.PostEliteserienFDRData.as_view()),
+    
+    # fill eliteserien db with kick of times and team info - EliteserienTeamInfo & EliteserienKickOffTime
+    # path("fill-eliteserien-kick-off-times-and-team-info-db/", views.FillEliteserienKickOffTimesAndTeamInfoDB),
 ]
