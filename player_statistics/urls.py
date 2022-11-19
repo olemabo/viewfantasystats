c703a176672b5ac0ad13e3bba7f2605170e470f7
Ole Martin Borge <olemartinbo@gmail.com>
2022-11-19 23:09:15 +0100

    remove urls to db updates

--- player_statistics/urls.py
+++ player_statistics/urls.py
@@ -15,22 +15,22 @@ urlpatterns = [
 
 # path to read and fill db (uses the fpl and eliteserien api's! )
 # Comment out this in PROD
-urlpatterns += [
-    # cup info
-    path('read-and-fill-db-cup-info-eliteserien/', views_db.read_and_fill_cup_info_to_db_eliteserien, name='read_and_write_cup_info_to_db_eliteserien'),
+# urlpatterns += [
+#     # cup info
+#     path('read-and-fill-db-cup-info-eliteserien/', views_db.read_and_fill_cup_info_to_db_eliteserien, name='read_and_write_cup_info_to_db_eliteserien'),
 
-    # user info
-    path('read-and-fill-db-user-info-eliteserien/', views_db.read_and_fill_user_info_to_db_eliteserien, name='read_and_write_user_info_to_db_eliteserien'),
-    # path('fill-db-user-info-eliteserien/', views.fill_user_info_to_db_eliteserien, name='write_user_info_to_db_eliteserien'),
+#     # user info
+#     path('read-and-fill-db-user-info-eliteserien/', views_db.read_and_fill_user_info_to_db_eliteserien, name='read_and_write_user_info_to_db_eliteserien'),
+#     # path('fill-db-user-info-eliteserien/', views.fill_user_info_to_db_eliteserien, name='write_user_info_to_db_eliteserien'),
     
-    # # global stats
-    path('read-and-fill-db-global-stats-eliteserien/', views_db.read_and_fill_db_global_stats, name='read_and_fill_txt_global_stats_eliteserien'),
-    path('fill-db-global-stats-eliteserien/', views_db.fill_db_global_stats, name='fill_txt_global_stats_eliteserien'),
+#     # # global stats
+#     path('read-and-fill-db-global-stats-eliteserien/', views_db.read_and_fill_db_global_stats, name='read_and_fill_txt_global_stats_eliteserien'),
+#     path('fill-db-global-stats-eliteserien/', views_db.fill_db_global_stats, name='fill_txt_global_stats_eliteserien'),
     
-    path('read-and-fill-db-global-stats-premier-league/', views_db.read_and_fill_db_global_stats_premier_league, name='read_and_fill_txt_global_stats_premier_league'),
-    # path('fill-db-global-stats-premier-league/', views_db.fill_db_global_stats_premier_league, name='fill_txt_global_stats_premier_league'),
+#     path('read-and-fill-db-global-stats-premier-league/', views_db.read_and_fill_db_global_stats_premier_league, name='read_and_fill_txt_global_stats_premier_league'),
+#     # path('fill-db-global-stats-premier-league/', views_db.fill_db_global_stats_premier_league, name='fill_txt_global_stats_premier_league'),
     
-    # # player stats
-    path('read-and-fill-db-player-statistics-eliteserien/', views_db.fill_player_statistics_eliteserien, name='read_and_fill_player_statistics_eliteserien'),
-    path('read-and-fill-db-player-statistics-premier-league/', views_db.fill_player_statistics_premier_league, name='read_and_fill_player_statistics_premier_league'),
-]
+#     # # player stats
+#     path('read-and-fill-db-player-statistics-eliteserien/', views_db.fill_player_statistics_eliteserien, name='read_and_fill_player_statistics_eliteserien'),
+#     path('read-and-fill-db-player-statistics-premier-league/', views_db.fill_player_statistics_premier_league, name='read_and_fill_player_statistics_premier_league'),
+# ]
