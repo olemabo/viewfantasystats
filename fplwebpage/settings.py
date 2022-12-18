d5313f652b2ab4bd8693c7be54466a37b7743634
Ole Martin Borge <olemartinbo@gmail.com>
2022-12-18 12:18:00 +0100

    rydding

--- fplwebpage/settings.py
+++ fplwebpage/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from decouple import config 
 from pathlib import Path
 import os
 
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = '%2gadogx94&wzo0#-c^h77$&)6_y93jby(1t_ukij(u!gn(e3u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
