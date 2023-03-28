14ec69878cf070583aa0431ad8ada74429d0a5d8
Ole Martin Borge <olemartinbo@gmail.com>
2023-03-28 20:31:19 +0200

    omstrukturer funksjoner til filer

--- player_statistics/backend/read_api_data_to_txt/read_player_statistics.py
+++ player_statistics/backend/read_api_data_to_txt/read_player_statistics.py
@@ -1,6 +1,7 @@
 from utils.utility_functions import get_static_json_data
 from constants import premier_league_api_url
 
+
 def get_ids(use_api_or_local, api_url=premier_league_api_url):
     """
         Extract and return a list of all fpl player ids
@@ -8,7 +9,9 @@ def get_ids(use_api_or_local, api_url=premier_league_api_url):
     static_info = get_static_json_data(use_api_or_local, api_url)['elements']
     ids = []
     for info in static_info:
-        ids.append(info['id'])
+        status = info['status']
+        if status != "u":
+            ids.append(info['id'])
 
     return ids
 
@@ -22,42 +25,54 @@ def static_json(use_api_or_local="local", api_url=premier_league_api_url):
     player_dict = dict()
 
     for player in elements:
-        player_dict[player['id']] = [player['first_name'],
-                                     player['second_name'],
-                                     player['element_type'],
-                                     player['chance_of_playing_this_round'],
-                                     player['now_cost'],
-                                     player['points_per_game'],
-                                     player['selected_by_percent'],
-                                     player['total_points'],
-                                     player['minutes'],
-                                     player['goals_scored'],
-                                     player['assists'],
-                                     player['clean_sheets'],
-                                     player['goals_conceded'],
-                                     player['own_goals'],
-                                     player['penalties_saved'],
-                                     player['penalties_missed'],
-                                     player['yellow_cards'],
-                                     player['red_cards'],
-                                     player['saves'],
-                                     player['bonus'],
-                                     player['bps'] if api_url == premier_league_api_url else 0,
-                                     player['influence'] if api_url == premier_league_api_url else 0,
-                                     player['creativity'] if api_url == premier_league_api_url else 0,
-                                     player['threat'] if api_url == premier_league_api_url else 0,
-                                     player['ict_index'] if api_url == premier_league_api_url else 0,
-                                     player['influence_rank'] if api_url == premier_league_api_url else 0,
-                                     player['influence_rank_type'] if api_url == premier_league_api_url else 0,
-                                     player['creativity_rank'] if api_url == premier_league_api_url else 0,
-                                     player['creativity_rank_type'] if api_url == premier_league_api_url else 0,
-                                     player['threat_rank'] if api_url == premier_league_api_url else 0,
-                                     player['threat_rank_type'] if api_url == premier_league_api_url else 0,
-                                     player['ict_index_rank'] if api_url == premier_league_api_url else 0,
-                                     player['ict_index_rank_type'] if api_url == premier_league_api_url else 0,
-                                     player['transfers_in'],
-                                     player['transfers_out'],
-                                     player['team'],
-                                     player['web_name']
-                                     ]
+        player_dict[player['id']] = [
+            player['first_name'], # 0
+            player['second_name'], # 1
+            player['element_type'], # 2
+            player['chance_of_playing_this_round'], # 3
+            player['now_cost'], # 4
+            player['points_per_game'], # 5
+            player['selected_by_percent'], # 6
+            player['total_points'], # 7
+            player['minutes'], # 8
+            player['goals_scored'], # 9
+            player['assists'], # 10
+            player['clean_sheets'], # 11
+            player['goals_conceded'], # 12
+            player['own_goals'], # 13
+            player['penalties_saved'], # 14
+            player['penalties_missed'], # 15
+            player['yellow_cards'], # 16
+            player['red_cards'], # 17
+            player['saves'], # 18
+            player['bonus'], # 19
+            player['bps'] if api_url == premier_league_api_url else 0, # 20
+            player['influence'] if api_url == premier_league_api_url else 0, # 21
+            player['creativity'] if api_url == premier_league_api_url else 0, # 22
+            player['threat'] if api_url == premier_league_api_url else 0, # 23
+            player['ict_index'] if api_url == premier_league_api_url else 0, # 24
+            player['influence_rank'] if api_url == premier_league_api_url else 0, # 25
+            player['influence_rank_type'] if api_url == premier_league_api_url else 0, # 26
+            player['creativity_rank'] if api_url == premier_league_api_url else 0, # 27
+            player['creativity_rank_type'] if api_url == premier_league_api_url else 0, # 28
+            player['threat_rank'] if api_url == premier_league_api_url else 0, # 29
+            player['threat_rank_type'] if api_url == premier_league_api_url else 0, # 30
+            player['ict_index_rank'] if api_url == premier_league_api_url else 0, # 31
+            player['ict_index_rank_type'] if api_url == premier_league_api_url else 0, # 32
+            player['transfers_in'], # 33
+            player['transfers_out'], # 34
+            player['team'], # 35
+            player['web_name'], # 36 
+            player['expected_goals'] if api_url == premier_league_api_url else 0, # 37
+            player['expected_goals_per_90'] if api_url == premier_league_api_url else 0, # 38
+            player['expected_assists'] if api_url == premier_league_api_url else 0, # 39
+            player['expected_assists_per_90'] if api_url == premier_league_api_url else 0, # 40
+            player['expected_goal_involvements'] if api_url == premier_league_api_url else 0, # 41
+            player['expected_goal_involvements_per_90'] if api_url == premier_league_api_url else 0, # 42
+            player['expected_goals_conceded'] if api_url == premier_league_api_url else 0, # 43
+            player['expected_goals_conceded_per_90'] if api_url == premier_league_api_url else 0, # 44
+            player['goals_conceded_per_90'] if api_url == premier_league_api_url else 0, # 45
+            player['saves_per_90'] if api_url == premier_league_api_url else 0, # 46
+        ]
+
     return player_dict
--- player_statistics/scripts/read_and_fill_user_info_to_db_eliteserien.py
+++ player_statistics/scripts/read_and_fill_user_info_to_db_eliteserien.py
@@ -1,7 +1,4 @@
-import numpy as np
 import django
-import json
-import time
 import sys
 import os
 
@@ -12,160 +9,14 @@ sys.path.append(prod)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fplwebpage.settings')
 django.setup()
 
-from constants import current_season_name_premier_league, current_season_name_eliteserien, path_to_store_local_data
-from constants import ranking_delimiter, user_stats_special_delimiter, user_stats_special_delimiter
-from constants import user_stats_folder_name, eliteserien_folder_name, user_stats_txt_file_name
-from constants import premier_league_api_url, eliteserien_api_url
 
-from player_statistics.db_models.eliteserien.user_statistics_model_eliteserien import EliteserienUserInfoStatistics
-from utils.dataFetch.DataFetch import DataFetch
-
-
-def read_user_info_statistics_eliteserien(league_name=eliteserien_folder_name, max_time=60 * 60 * 24):
-    api_url = eliteserien_api_url if league_name == eliteserien_folder_name else premier_league_api_url
-    DFObject = DataFetch(api_url)
-    
-    # create a folder to store data in txt files, and return data if allready exists
-    start_id, path_to_file = check_if_txt_file_exist(league_name)
-    print("\n\nRead data from API | User statistics\n")
-    print("Start at id: ", start_id, "\n")
-    
-    # get number of fantasy players
-    number_of_fantasy_players = DFObject.get_current_fpl_info()['total_players']
-    
-    print("Number of fantasy managers: ", number_of_fantasy_players, "\n")
-    
-    if start_id > number_of_fantasy_players:
-        print("All ", number_of_fantasy_players, " managers allready stored :)")
-        return 0
-
-    total_time = 0
-    for id in range(start_id, number_of_fantasy_players + 1):
-        start_time = time.time()
-        content = read_single_user_info(id, DFObject)
-        wrtie_to_file(content, path_to_file)
-        end_time = time.time()
-        total_time += (end_time - start_time) 
-        avg_time = total_time / (id - start_id + 1)
-        sec_left = avg_time * (number_of_fantasy_players - id)
-        if (sec_left < 60):
-            print(id, " / ", number_of_fantasy_players, " | ", round(total_time, 2), "(s) | ", round(id / number_of_fantasy_players * 100, 2) , "% | " , round(sec_left, 2), " secs left")
-        if (sec_left < 60 * 60):
-            print(id, " / ", number_of_fantasy_players, " | ", round(total_time, 2), "(s) | ", round(id / number_of_fantasy_players * 100, 2) , "% | " , round(sec_left / 60, 2), " mins left")
-        else:
-            print(id, " / ", number_of_fantasy_players, " | ", round(total_time, 2), "(s) | ", round(id / number_of_fantasy_players * 100, 2) , "% | " , round(sec_left / 3600, 2), " hours left")
-
-        if (total_time  > max_time):
-            return -1
-
-    return 0
-
-
-
-def check_if_txt_file_exist(league_name):
-    league_path = path_to_store_local_data + "/" + league_name + "/"
-    if not os.path.isdir(league_path):
-        os.mkdir(league_path)
-
-    season_name = current_season_name_eliteserien if league_name == eliteserien_folder_name else current_season_name_premier_league
-    season_path = league_path + "/" + season_name + "/"
-    if not os.path.isdir(season_path):
-        os.mkdir(season_path)
-
-    user_stat_path = season_path + "/" + user_stats_folder_name
-    if not os.path.isdir(user_stat_path):
-        os.mkdir(user_stat_path)
-
-    txt_file_path = user_stat_path + "/" + user_stats_folder_name + ".txt"
-    if not os.path.exists(txt_file_path):
-        f = open(txt_file_path, "w", encoding="utf-8")
-        f.write("id, joined_time, started_event, favourite_team, name,  player_first_name, player_last_name, player_region_id, player_region_name, player_region_iso_code_long, ranking \n")
-        f.close()
-
-    start_id = 0
-    data_allready_stored = np.loadtxt(txt_file_path, dtype="str", delimiter="\n", skiprows=1, encoding="utf-8")
-    for data in data_allready_stored:
-        start_id = max(start_id, int(data.split(user_stats_special_delimiter)[0]))
-    
-    return start_id + 1, txt_file_path
-
-
-
-def read_single_user_info(id, DFObject: DataFetch):
-    time.sleep(1.2)
-    
-    current_fpl_user = DFObject.get_current_fpl_player(id)
-    current_fpl_user_history = DFObject.get_current_member(id)['past']
-    
-    history_data_str = "["
-    for idx, history_data in enumerate(current_fpl_user_history):
-        history_data_str += "[" + str(history_data['season_name']) + "," + str(history_data['total_points']) + "," + str(history_data['rank']) + "]"
-        if (idx != len(current_fpl_user_history) - 1):
-            history_data_str += ","
-    history_data_str += "]"
-    
-    return str(current_fpl_user['id']) + user_stats_special_delimiter + str(current_fpl_user['joined_time']) + user_stats_special_delimiter \
-        + str(current_fpl_user['started_event']) + user_stats_special_delimiter + str(current_fpl_user['favourite_team']) + user_stats_special_delimiter \
-        + str(current_fpl_user['name']) + user_stats_special_delimiter + str(current_fpl_user['player_first_name']) + user_stats_special_delimiter \
-        + str(current_fpl_user['player_last_name']) + user_stats_special_delimiter + str(current_fpl_user['player_region_id']) + user_stats_special_delimiter  \
-        + str(current_fpl_user['player_region_name']) + user_stats_special_delimiter + str(current_fpl_user['player_region_iso_code_long']) + user_stats_special_delimiter \
-        + history_data_str
-
-
-def wrtie_to_file(content, path_to_file):
-    f2 = open(path_to_file, "a", encoding="utf-8")
-    f2.write(content + "\n")
-    f2.close()
-
-
-def write_user_info_to_db_eliteserien():
-    path = path_to_store_local_data + "/" + eliteserien_folder_name + "/" + current_season_name_eliteserien + "/" + user_stats_folder_name + "/" + user_stats_txt_file_name
-    user_info_data = np.loadtxt(path, dtype="str", comments="&&&&&&&&&&&", delimiter=user_stats_special_delimiter, skiprows=1, encoding="utf-8")
-    new = 0
-    update = 0
-    print("\n")
-    for idx, data in enumerate(user_info_data):
-        id = data[0]
-        started_event = data[2]
-        favourite_team = data[3]
-        team_name = data[4]
-        player_first_name = data[5]
-        player_last_name = data[6]
-        player_region_id = data[7]
-        player_region_name = data[8]
-        player_region_iso_code_long = data[9]
-        ranking_list = json.loads(data[10])
-        joined_time = data[1]
-        ranking_list2 = []
-        for i in ranking_list:
-            list_as_string = str(i).replace(",", ranking_delimiter).replace(" ", "").replace("[", "").replace("]", "")
-            ranking_list2.append(list_as_string)
-        
-        if len(EliteserienUserInfoStatistics.objects.filter(user_id = id)) > 0:
-            update += 1 
-            fill_model = EliteserienUserInfoStatistics.objects.filter(user_id = id)
-            fill_model.update(user_team_name=team_name, 
-                    user_first_name=player_first_name, 
-                    user_last_name=player_last_name,
-                    ranking_history=ranking_list2,
-                    )
-        else:
-            new += 1
-            fill_model = EliteserienUserInfoStatistics(
-                user_id=id,
-                user_team_name=team_name, 
-                user_first_name=player_first_name, 
-                user_last_name=player_last_name,
-                ranking_history=ranking_list2)
-            fill_model.save()
-
-        if (idx % 100 == 0):
-            print(idx, " / ", len(user_info_data), "(", new, "/", update, ")", " (new/update)")
-        
+from player_statistics.backend.read_api_data_to_txt.read_user_info_statistics import read_user_info_statistics_eliteserien
+from player_statistics.backend.fill_db_from_txt.fill_db_user_info_statistics import write_user_info_to_db_eliteserien
+from constants import eliteserien_folder_name
 
 
 if __name__ == '__main__':
-    number_of_minutes_to_run = 1
+    number_of_minutes_to_run = 5
     response = read_user_info_statistics_eliteserien(eliteserien_folder_name, 60 * number_of_minutes_to_run)
     print("\nNot finished scraping all user info to txt files") if response == -1 else print("\nFinished scraping all user info to txt files")
     
